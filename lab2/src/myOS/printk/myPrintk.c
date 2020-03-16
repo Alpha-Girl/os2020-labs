9ec44919fcacf6c640ce49ebb4ae1ba49dd8ec6c
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-03-16 18:16:38 +0800

    edit myPrintk.c

--- lab2/src/myOS/printk/myPrintk.c
+++ lab2/src/myOS/printk/myPrintk.c
@@ -1,11 +1,17 @@
 //实现myPrint功能，需要调用到格式化输出的function（vsprintf）
 extern void append2screen(char *str,int color);
 extern void uart_put_chars(char *str);
-
+extern int vsprintf(char *buf, const char *fmt, ...);
 char kBuf[400];  //TODO: fix me
-int myPrintk(int color,const char *format, ...){ 
+int myPrintk(int color,const char *format, ...){
+    vsprintf(kBuf,format);
+    uart_put_chars(kBuf);
+    return 0;
 }
 
 char uBuf[400];  //TODO: fix me
 int myPrintf(int color,const char *format, ...){
+    vsprintf(uBuf,format);
+    append2screen(uBuf,color);
+    return 0;
 }
