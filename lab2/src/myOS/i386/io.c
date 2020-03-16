6b48f8d097c0582084d39a268e3beb6c6cffa0d3
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-03-16 12:00:33 +0800

    edit io.c

--- lab2/src/myOS/i386/io.c
+++ lab2/src/myOS/i386/io.c
@@ -12,3 +12,7 @@ unsigned char inb(unsigned short int port_from){
 
     return _in_value;
 }
+
+void outb (unsigned short int port_to, unsigned char value){
+    __asm__ __volatile__ ("outb %b0,%w1"::"a" (value),"Nd" (port_to));
+}
