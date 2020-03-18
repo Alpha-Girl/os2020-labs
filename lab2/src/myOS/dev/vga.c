c48020d8efe5c1040b57513ac75c00b9d7282aae
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-03-18 08:23:06 +0800

    edit code style of vga.c

--- lab2/src/myOS/dev/vga.c
+++ lab2/src/myOS/dev/vga.c
@@ -3,60 +3,57 @@
 extern int myPrintk(int color,const char *format, ...);
 extern void outb (unsigned short int port_to, unsigned char value);
 extern unsigned char inb(unsigned short int port_from);
+void move(void);
 #define vga_base 0xB8000
+#define srceen_width 80
+#define srceen_height 25
 unsigned short int *port,*tmp;
-unsigned int p,st;
+unsigned int p;
 unsigned char row=0,col=0;
-void wr_row(unsigned char in_row);
-void wr_col(unsigned char in_col);
-unsigned char gt_row(unsigned char out_row);
-unsigned char gt_col(unsigned char out_col);
+
 void clear_screen(void) {
     int i;
-    int a;
-    a=25*80;
-    port=vga_base;
-    p=vga_base;
-    for(i = 0; i < a; i++)
+    port = vga_base;
+    p = vga_base;
+    for(i = 0; i < srceen_width * srceen_height; i++)
     {
         
         *port = 0x0020;
-        p=p+2;
-        port=p;
+        p = p + 2;
+        port = p;
     }
-    p=vga_base;	
+    p = vga_base;	
 }
 
 void append2screen(char *str,int color){
     int i,j;
     unsigned short int output;
-    //disable_interrupt();
     for(i = 0; ; i++)
     {
         if(str[i] != '\0')
         {
-            if(str[i]!='\n')
+            if(str[i] != '\n')
             {
-                if(row==25)
+                if(row == srceen_height)
                 {
                     move();
-                    row=24;
-                    col=0;
+                    row = 24;
+                    col = 0;
                 }
-                output = color*16*16+str[i];
-                port=vga_base+row*160+col*2;
+                output = color * 16 * 16 + str[i];
+                port = vga_base + row * 2 * srceen_width +col*2;
                 *port = output;
                 col++;
-                if(col>79)
+                if(col > (srceen_width - 1))
                 {
-                row++;
-                col=0;
+                    row++;
+                    col = 0;
                 }
             }
             else
             {
                 row++;
-                col=0;
+                col = 0;
             }
             
         }
@@ -66,7 +63,6 @@ void append2screen(char *str,int color){
         }
         
     }
-    //enable_interrupt();
 }
 void move()
 {
