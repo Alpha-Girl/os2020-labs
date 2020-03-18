e9120f035afd83a1c4e7858e0b5c30f0a3c25b2e
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-03-18 11:39:03 +0800

    edit main.c

--- lab2/src/userApp/main.c
+++ lab2/src/userApp/main.c
@@ -1,10 +1,10 @@
+
 extern int myPrintf(int color,const char *format, ...);
 
 void myMain(void){    
     int i;
 
     myPrintk(0x7,"main\n");
-    myPrintk(0x7,"%d\n",1);
     for (i=1;i<30;i++) myPrintf(i,"%d\n",i);
     return;
 }
