1f61744ef0b4e6455960ff5dc5f89f2969779734
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-03-19 19:03:48 +0800

    cursor pass!!

--- lab2/src/myOS/dev/vga.c
+++ lab2/src/myOS/dev/vga.c
@@ -10,6 +10,12 @@ void move(void);
 unsigned short int *port,*tmp;
 unsigned int p;
 unsigned char row=0,col=0;
+unsigned char rd_row(void);
+unsigned char rd_col(void);
+void wr_row(unsigned char row_in);
+void wr_col(unsigned char col_in);
+void wr_cursor(unsigned char row_in,unsigned char col_in);
+
 
 void clear_screen(void) {
     int i;
@@ -18,15 +24,18 @@ void clear_screen(void) {
     for(i = 0; i < srceen_width * srceen_height; i++)
     {
         
-        *port = 0x0020;
+        *port = 0x0f20;
         p = p + 2;
         port = p;
     }
-    p = vga_base;	
+    p = vga_base;
+    row = 0;
+    col = 0;
+    //wr_cursor(row,col);	
 }
 
 void append2screen(char *str,int color){
-    int i,j;
+    int i,j,c;
     unsigned short int output;
     for(i = 0; ; i++)
     {
@@ -34,12 +43,7 @@ void append2screen(char *str,int color){
         {
             if(str[i] != '\n')
             {
-                if(row == srceen_height)
-                {
-                    move();
-                    row = 24;
-                    col = 0;
-                }
+                
                 output = color * 16 * 16 + str[i];
                 port = vga_base + row * 2 * srceen_width +col*2;
                 *port = output;
@@ -61,7 +65,15 @@ void append2screen(char *str,int color){
         {
             break;
         }
-        
+        if(row == srceen_height)
+            {
+                    move();
+                    row = 24;
+                    col = 0;
+            }
+        //myPrintk(0x7,"row: %d, col: %d\n",rd_row(),rd_col());
+        c=row*80+col;
+        wr_cursor(c/256,c%256);
     }
 }
 void move()
@@ -81,6 +93,30 @@ void move()
     for(j=0;j<80;j++)
     {
         tmp=vga_base+24*160+2*j;
-        *tmp=0x0020;
+        *tmp=0x0f20;
     }
+}
+
+unsigned char rd_row(void){
+    outb(0x3D4,0xE);
+    return inb(0x3D5);
+}
+
+unsigned char rd_col(void){
+    outb(0x3D4,0xF);
+    return inb(0x3D5);
+}
+
+void wr_row(unsigned char row_in){
+    outb(0x3D4,(unsigned char)0xE);
+    outb(0x3D5,row_in);
+}
+void wr_col(unsigned char col_in){
+    outb(0x3D4,(unsigned char)0xF);
+    outb(0x3D5,col_in);
+}
+
+void wr_cursor(unsigned char row_in,unsigned char col_in){
+    wr_row(row_in);
+    wr_col(col_in);
 }
