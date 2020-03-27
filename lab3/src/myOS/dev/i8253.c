578aef8fd4b081dfaa583f681c1473328ed9a7e1
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-03-27 23:42:25 +0800

    feat(i8253.c): add init8253

---
+++ lab3/src/myOS/dev/i8253.c
@@ -0,0 +1,13 @@
+extern void outb (unsigned short int port_to, unsigned char value);
+extern unsigned char inb(unsigned short int port_from);
+
+void init8253(void)
+{
+    unsigned char c;
+	outb(0x43,0x34);
+    outb(0x40,0x9c);
+    outb(0x40,0x2e);
+    c=inb(0x21);
+    c=c/2;
+    outb(0x21,c);
+}
