86551341a8840a00443d9b257e4be73e4e88a6ac
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-06-07 11:17:26 +0800

    style(task.h)

--- lab5/src/myOS/include/task.h
+++ lab5/src/myOS/include/task.h
@@ -5,29 +5,30 @@
 #include "../../userApp/userApp.h"
 #endif
 
-#define TASK_NUM (2 + USER_TASK_NUM)   // at least: 0-idle, 1-init
+#define TASK_NUM (2 + USER_TASK_NUM) // at least: 0-idle, 1-init
 
-#define initTskBody myMain         // connect initTask with myMain
+#define initTskBody myMain // connect initTask with myMain
 
 void initTskBody(void);
 #define STACK_SIZE 4096
-void CTX_SW(void*prev_stkTop, void*next_stkTop);
+void CTX_SW(void *prev_stkTop, void *next_stkTop);
 int createTsk(void (*tskBody)(void));
 void destroyTsk(int tskIndex);
 void tskEnd(void);
-typedef struct myTCB {
-     unsigned long *stkTop;     /* 栈顶指针 */
-     unsigned long tcbIndex;
-     unsigned long *stack;
-     unsigned long state;
-     struct myTCB *next;
+typedef struct myTCB
+{
+     unsigned long *stkTop;  /* 栈顶指针 */
+     unsigned long tcbIndex; /* 任务ID */
+     unsigned long *stack;   /* 栈底指针 */
+     unsigned long state;    /* 任务状态 */
+     struct myTCB *next;     /* 下一任务 */
 } myTCB;
 
 myTCB tcbPool[TASK_NUM];
 
-myTCB * idleTsk;                /* idle 任务 */
-myTCB * currentTsk;             /* 当前任务 */
-myTCB * firstFreeTsk;           /* 下一个空 TCB */
+myTCB *idleTsk;      /* idle 任务 */
+myTCB *currentTsk;   /* 当前任务 */
+myTCB *firstFreeTsk; /* 下一个空 TCB */
 
 void TaskManagerInit(void);
 
